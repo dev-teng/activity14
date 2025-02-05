cfda2b1943610e999649172e3b63520687977d12
dev-teng <vnctlcrn@gmail.com>
2025-02-05 17:00:51 +0800

    added pages and imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 function App() {
 
   return (
     <>
-    <h1>Hello World</h1>
+   
     </>
   )
 }
