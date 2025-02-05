6de16d0312a0a405f20964e28d05a7798298987e
dev-teng <vnctlcrn@gmail.com>
2025-02-05 15:55:53 +0800

    start in adding bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+function App() {
+
+  return (
+    <>
+    <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
